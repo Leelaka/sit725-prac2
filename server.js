fdd7840e044e5ddf678e8f91f9c2c0fb90c1a0d4
Leelaka Adassuriya <37642488+Leelaka@users.noreply.github.com>
2020-07-26 21:56:31 +1000

    inprogress

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.get('/test', function(){
+    console.log()
+});
+var port = 3000;
+app.listen(port);
+console.log('Server listening on: '+ port);
